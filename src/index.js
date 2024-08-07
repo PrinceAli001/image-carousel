1b339d358ede7e1f696dec147c9485b224734f0f
Mahfouz <mafz060706@gmail.com>
2024-08-07 18:28:14 +0100

    All dots and left arrow are functional
    The dots can be used to move to their respective
    images.
    The left arrow sends the carousel one image back.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Girl from '../Images/girl.webp';
 import Logo from '../Images/logo.png';
 import Left from '../Images/left-arrow.png';
 import Right from '../Images/right-arrow.png';
+import moveImages from './moveImages';
 
 display();
 
@@ -17,6 +18,11 @@ function display() {
     let divFive = document.querySelector('#div-five');
     let leftArrow = document.querySelector('#left-arrow');
     let rightArrow = document.querySelector('#right-arrow');
+    let dotOne = document.querySelector('#dot-one');
+    let dotTwo = document.querySelector('#dot-two');
+    let dotThree = document.querySelector('#dot-three');
+    let dotFour = document.querySelector('#dot-four');
+    let dotFive = document.querySelector('#dot-five');
     let imgOne = document.createElement('img');
     let imgTwo = document.createElement('img');
     let imgThree = document.createElement('img');
@@ -24,6 +30,10 @@ function display() {
     let imgFive = document.createElement('img');
     let imgSix = document.createElement('img');
     let imgSeven = document.createElement('img');
+    let arr = ['one','two','three','four','five'];
+    let changedNumber = 0;
+    let currentPosition;
+    
 
     imgOne.src = Bishop;
     imgTwo.src = Douche;
@@ -40,4 +50,53 @@ function display() {
     divFive.appendChild(imgFive);
     leftArrow.appendChild(imgSix);
     rightArrow.appendChild(imgSeven);
+
+    dotOne.addEventListener('click', () => {
+        moveImages('five');
+        changedNumber = 0;
+    });
+    dotTwo.addEventListener('click', () => {
+        moveImages('one');
+        changedNumber = 1;
+    });
+    dotThree.addEventListener('click', () => {
+        moveImages('two');
+        changedNumber = 2;
+    });
+    dotFour.addEventListener('click', () => {
+        moveImages('three');
+        changedNumber = 3;
+    });
+    dotFive.addEventListener('click', () => {
+        moveImages('four');
+        changedNumber = 4;
+    });
+    leftArrow.addEventListener('click', () => {
+        changedNumber-= 2;
+        currentPosition = arr[changedNumber];
+        moveImages(currentPosition);
+
+        if (changedNumber < 0) {
+            changedNumber = 0
+        };
+    });
+    rightArrow.addEventListener('click', () => {
+        changedNumber++;
+        currentPosition = arr[changedNumber];
+        moveImages(currentPosition);
+
+        if (changedNumber > 4) {
+            changedNumber = 4
+        };
+    });
+
+    setInterval(() => {
+        currentPosition = arr[changedNumber];
+        moveImages(currentPosition);
+        changedNumber++;
+
+        if (changedNumber > 4) {
+            changedNumber = 0
+        };
+    }, 3000);
 }
---
+++ src/moveImages.js
@@ -0,0 +1,88 @@
+export default function moveImages(postion) {
+    let divOne = document.querySelector('#div-one');
+    let divTwo = document.querySelector('#div-two');
+    let divThree = document.querySelector('#div-three');
+    let divFour = document.querySelector('#div-four');
+    let divFive = document.querySelector('#div-five');
+    let dotOne = document.querySelector('#dot-one');
+    let dotTwo = document.querySelector('#dot-two');
+    let dotThree = document.querySelector('#dot-three');
+    let dotFour = document.querySelector('#dot-four');
+    let dotFive = document.querySelector('#dot-five');
+
+    switch (postion) {
+        case'one':
+            divOne.setAttribute('style','grid-area: 1/4/2/5; display: none;');
+            divTwo.setAttribute('style','grid-area: 1/5/2/6;');
+            divThree.setAttribute('style','grid-area: 1/6/2/7; display: none;');
+            divFour.setAttribute('style','grid-area: 1/7/2/8; display: none;');
+            divFive.setAttribute('style','grid-area: 1/8/2/9; display: none;');
+
+            dotOne.setAttribute('style','height: 8px; width: 8px; background: none;');
+            dotTwo.setAttribute('style','height: 12px; width: 12px; background-color: #ffa500;');
+            dotThree.setAttribute('style','height: 8px; width: 8px;');
+            dotFour.setAttribute('style','height: 8px; width: 8px;');
+            dotFive.setAttribute('style','height: 8px; width: 8px;');
+            break;
+        case'two':
+            divOne.setAttribute('style','grid-area: 1/3/2/4; display: none;');
+            divTwo.setAttribute('style','grid-area: 1/4/2/5; display: none;');
+            divThree.setAttribute('style','grid-area: 1/5/2/6;');
+            divFour.setAttribute('style','grid-area: 1/6/2/7; display: none;');
+            divFive.setAttribute('style','grid-area: 1/7/2/8; display: none;');
+
+            dotOne.setAttribute('style','height: 8px; width: 8px; background: none;');
+            dotTwo.setAttribute('style','height: 8px; width: 8px;');
+            dotThree.setAttribute('style','height: 12px; width: 12px; background-color: #ffa500;');
+            dotFour.setAttribute('style','height: 8px; width: 8px;');
+            dotFive.setAttribute('style','height: 8px; width: 8px;');
+            break;
+        case'three':
+            divOne.setAttribute('style','grid-area: 1/2/2/3; display: none;');
+            divTwo.setAttribute('style','grid-area: 1/3/2/4; display: none;');
+            divThree.setAttribute('style','grid-area: 1/4/2/5; display: none;');
+            divFour.setAttribute('style','grid-area: 1/5/2/6;');
+            divFive.setAttribute('style','grid-area: 1/6/2/7; display: none;');
+
+            dotOne.setAttribute('style','height: 8px; width: 8px; background: none;');
+            dotTwo.setAttribute('style','height: 8px; width: 8px;');
+            dotThree.setAttribute('style','height: 8px; width: 8px;');
+            dotFour.setAttribute('style','height: 12px; width: 12px; background-color: #ffa500;');
+            dotFive.setAttribute('style','height: 8px; width: 8px;');
+            break;
+        case'four':
+            divOne.setAttribute('style','grid-area: 1/1/2/2; display: none;');
+            divTwo.setAttribute('style','grid-area: 1/2/2/3; display: none;');
+            divThree.setAttribute('style','grid-area: 1/3/2/4; display: none;');
+            divFour.setAttribute('style','grid-area: 1/4/2/5; display: none;');
+            divFive.setAttribute('style','grid-area: 1/5/2/6;');
+
+            dotOne.setAttribute('style','height: 8px; width: 8px; background: none;');
+            dotTwo.setAttribute('style','height: 8px; width: 8px;');
+            dotThree.setAttribute('style','height: 8px; width: 8px;');
+            dotFour.setAttribute('style','height: 8px; width: 8px;');
+            dotFive.setAttribute('style','height: 12px; width: 12px; background-color: #ffa500;');
+            break;
+        case'five':
+            divOne.setAttribute('style','grid-area: 1/5/2/6;');
+            divTwo.setAttribute('style','grid-area: 1/6/2/7; display: none;');
+            divThree.setAttribute('style','grid-area: 1/7/2/8; display: none;');
+            divFour.setAttribute('style','grid-area: 1/8/2/9; display: none;');
+            divFive.setAttribute('style','grid-area: 1/9/2/10; display: none;');
+
+            dotOne.setAttribute('style','height: 12px; width: 12px; background-color: #ffa500;');
+            dotTwo.setAttribute('style','height: 8px; width: 8px;');
+            dotThree.setAttribute('style','height: 8px; width: 8px;');
+            dotFour.setAttribute('style','height: 8px; width: 8px;');
+            dotFive.setAttribute('style','height: 8px; width: 8px;');
+            break;
+
+        default:
+            divOne.setAttribute('style','grid-area: 1/5/2/6;');
+            divTwo.setAttribute('style','grid-area: 1/6/2/7;');
+            divThree.setAttribute('style','grid-area: 1/7/2/8;');
+            divFour.setAttribute('style','grid-area: 1/8/2/9;');
+            divFive.setAttribute('style','grid-area: 1/9/2/10;');
+            break;
+    }
+}
